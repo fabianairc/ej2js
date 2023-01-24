4210fe3a4f802c759690fea7b7ea2ab66b821444
fabianairc <fabianaramirezzz@gmail.com>
2023-01-24 17:58:37 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const list = [
+  "fabiana",
+  30,
+  true,
+  new Date(1992, 5, 22),
+  {
+    titulo: "harry potter",
+    autor: "jl rowling",
+    fecha: 1194,
+    url: "ww.ss..ss",
+  },
+];
+console.log(list);
